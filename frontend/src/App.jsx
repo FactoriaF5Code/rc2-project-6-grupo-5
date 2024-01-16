4bc0ff21071b8119939be79ccf2df14010a02719
San-QP <s.rebollar.g@gmail.com>
2024-01-16 13:57:07 +0100

    Galería de hoteles creada con texto falso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Header } from './components/Header/Header'
+import { GalleryHotels } from "./components/GalleryHotels/GalleryHotels";
 
 function App() {
 
   return (
     <>
       <Header />
+      
+      <GalleryHotels />
     </>
   )
 }
