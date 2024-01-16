109b58a3bb8702a7db2c978dcedb196c8bd7b49d
San-QP <s.rebollar.g@gmail.com>
2024-01-16 12:30:42 +0100

    Componente Header creado

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+export const Header = () => {
+  return (
+    <header>
+        <nav>
+            <h1>Trooking</h1>
+            <button className="reservationButton">Mis reservas</button>
+        </nav>
+    </header>
+  )
+}
