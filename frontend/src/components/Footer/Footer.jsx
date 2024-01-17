5862e0a0496566dba20af54ff4086ea472fd29cf
San-QP <s.rebollar.g@gmail.com>
2024-01-17 10:46:44 +0100

    Estilos modificados en Home

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,22 +4,22 @@ import { useState } from "react";
 export const Footer = () => {
   const [email, setEmail] = useState("");
   return (
-    <section>
-      <h2>¡Ahorra tiempo y dinero!</h2>
-      <h3>Registrate y te enviaremos las mejores ofertas para </h3>
-      <form>
-        <input
+    <section className="footer">
+      <h2 className="footerTitle">¡Ahorra tiempo y dinero!</h2>
+      <p className="footerText">Registrate y te enviaremos las mejores ofertas para ti.</p>
+      <form className="footerForm">
+        <input className="footerEmailInput"
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Tu direccion de email"
         ></input>
-        <button className="suscribete">¡Suscribete!</button>
+        <button className="footerSubscribeButton">¡Suscribete!</button>
       </form>
-      <div className="footerlinks">
+      <div className="footerLinks">
         <a href="">Tu cuenta</a>
-        <a href="">Gestiona tus reservas onlin</a>
+        <a href="">Gestiona tus reservas online</a>
         <a href="">Atención al cliente</a>
         <a href="">Conviertete en afiliado</a>
         <a href="">Trooking.com for Business</a>
