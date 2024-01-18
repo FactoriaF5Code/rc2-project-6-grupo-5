8bd2890039bca053f120b0550b50c2a3a87c9925
evavelasco <evapibiyal@gmail.com>
2024-01-18 12:02:47 +0100

    snackbar sin rematar

--- frontend/src/components/HotelCard/HotelCard.jsx
+++ frontend/src/components/HotelCard/HotelCard.jsx
@@ -1,14 +1,26 @@
 import "./HotelCard.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import { HeartIcon } from "../Svg/HeartIcon";
 import { ModalReservationDates } from "../Modal/ModalReservationDates";
+import { Button, IconButton, Snackbar } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import Alert from "@material-ui/lab/Alert";
 
 export const HotelCard = (hotel) => {
-
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
+  const [snackOpen, setSnackOpen] = useState(false);
+  const [fechaEntrada, setFechaEntrada] = useState();
+  const [fechaSalida, setFechaSalida] = useState();
   const openModal = () => {
     setModalIsOpen(true);
+  };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSnackOpen(false);
   };
 
   return (
@@ -18,20 +30,59 @@ export const HotelCard = (hotel) => {
         <div className="hotelInfo">
           <div className="hotelDetails">
             <div className="hotelNamePrice">
-                <h3 className="hotelName">{hotel.name}</h3>
-                <p className="hotelPrice">{hotel.price} €</p>
+              <h3 className="hotelName">{hotel.name}</h3>
+              <p className="hotelPrice">{hotel.price} €</p>
             </div>
             <p className="hotelDescription">{hotel.description}</p>
           </div>
           <div className="reservationInfo">
-            <div className="icon"><HeartIcon /></div>
-            <button className="hotelReservationButton"  onClick={openModal}>
+            <div className="icon">
+              <HeartIcon />
+            </div>
+            <button className="hotelReservationButton" onClick={openModal}>
               ¡Reserva Ahora!
             </button>
-            {modalIsOpen && <ModalReservationDates closeModal={() => setModalIsOpen(false)} />}
+            {modalIsOpen && (
+              <ModalReservationDates
+                closeModal={() => {
+                  setSnackOpen(true);
+                  setModalIsOpen(false);
+                }}
+              />
+            )}
           </div>
         </div>
       </li>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        open={snackOpen}
+        
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Note archived"
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              UNDO
+            </Button>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      >
+        <Alert onClose={handleClose} severity="success">
+          Enhorabuena! Has reservado en {hotel.name} entre
+        </Alert>
+      </Snackbar>
     </>
   );
 };
--- frontend/src/components/Modal/ModalReservationDates.jsx
+++ frontend/src/components/Modal/ModalReservationDates.jsx
@@ -1,23 +1,44 @@
-import "./ModalReservationDates.css"
-import PropTypes from 'prop-types';
+import "./ModalReservationDates.css";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const ModalReservationDates = ({ closeModal }) => {
+  const [fechaEntrada, setFechaEntrada] = useState();
+  const [fechaSalida, setFechaSalida] = useState();
   return (
     <>
-        <section className="modalContainer">
-            <button className="modalCloseButton" onClick={closeModal}>CERRAR X</button>
-            <h2 className="modalTitle">Completa tu reserva</h2>
-            <form action="" className="modalForm">
-                <label htmlFor="fechaEntrada" className="modalFormLabel">Fecha de entrada</label>
-                <input type="date" id="fechaEntrada"></input>
-                <label htmlFor="fechaSalida" className="modalFormLabel">Fecha de salida</label>
-                <input type="date" id="fechaSalida"></input>
-            </form>
-            <button className="modalReservationButton">RESERVA AHORA</button>
-        </section>
+      <section className="modalContainer">
+        <button className="modalCloseButton" onClick={closeModal}>
+          CERRAR X
+        </button>
+        <h2 className="modalTitle">Completa tu reserva</h2>
+        <form action="" className="modalForm">
+          <label htmlFor="fechaEntrada" className="modalFormLabel">
+            Fecha de entrada
+          </label>
+          <input
+            type="date"
+            id="fechaEntrada"
+            value={fechaEntrada}
+            onChange={(e) => setFechaEntrada(e.target.value)}
+          ></input>
+          <label htmlFor="fechaSalida" className="modalFormLabel">
+            Fecha de salida
+          </label>
+          <input
+            type="date"
+            id="fechaSalida"
+            value={fechaSalida}
+            onChange={(e) => setFechaSalida(e.target.value)}
+          ></input>
+        </form>
+        <button className="modalReservationButton" onClick={closeModal}>
+          RESERVA AHORA
+        </button>
+      </section>
     </>
-  )
-}
+  );
+};
 
 ModalReservationDates.propTypes = {
   closeModal: PropTypes.func.isRequired,
