d8945bb4e3a8fc1f0b20c4504e46e2857d45eb1f
evavelasco <evapibiyal@gmail.com>
2024-01-18 15:13:10 +0100

    ampliacion datos

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,3 +4,39 @@ VALUES
 ('Hotel Campo Verde', 'Hotel tranquilo en el campo con vistas a la montaña, ideal para escapadas románticas', 'https://static.leonardo-hotels.com/image/P_LHBLR_05_may16_4000x2600_8254e8943e796f7d44a32ffd8d650a25_1200x780_mobile_3.webp', 99.89),
 ('Urbano Moderno', 'Hotel boutique en el centro de la ciudad con decoración moderna y un bar en la azotea', 'https://static.leonardo-hotels.com/image/p_lrhbf_02_nov21_-4000x2600_0039400c1259e6b7fe1a79ef368294b9_1200x780_mobile_3.webp', 199.20),
 ('Refugio de Montaña', 'Hotel en la montaña con acceso a senderos de trekking y vistas panorámicas', 'https://www.estiber.com/blog/wp-content/uploads/2023/07/refugio-lluis-estasen.jpg', 37.99);
+    
+         ("Almeida 1",
+        : "Encanto y comodidad en Almeida de Sayago, habitación individual.",
+         "https://imgcy.trivago.com/c_fill,d_dummy.jpeg,f_auto,h_190,q_auto,w_240//partner-images/c5/15/b5a03496961d97fb674c9b673e640b1badae39c36adcbdf4420c770236bf.jpeg",
+        90)
+   
+    ("Almeida 2",
+         "Encanto y comodidad en Almeida de Sayago, habitación doble.",
+         "https://cf.bstatic.com/xdata/images/hotel/max1024x768/5011469.jpg?k=89074ae1dae1cc2daa7c3b2c1f7fbad78d0a0a1fb43b30c6363828a2b24f8b68&o=&hp=1",
+         120)
+    ('Sunset Paradise Hotel', 'Hotel boutique en la costa con impresionantes vistas al atardecer, piscina infinita y spa de lujo', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/db/f9/eb/limanaki-hotel.jpg?w=1200&h=-1&s=1', 599.99),
+('Montaña Mágica Lodge', 'Retiro en la montaña rodeado de bosques, cabañas acogedoras y senderos naturales', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/d3/03/43/la-montana-magica.jpg?w=1200&h=-1&s=1', 349.99),
+('Oasis Sereno Resort', 'Resort de lujo con jardines exuberantes, spa rejuvenecedor y cocina gourmet', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partner-images/96/e2/4529aecaa4a6578881485c3078756ee3a31d7b6a7259962bf8887a523e19.jpeg', 499.99),
+('Aventura Marina Hotel', 'Experimenta la vida marina en este hotel frente al océano con actividades acuáticas y restaurantes temáticos', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partner-images/7d/d4/827377afcea0686d6a599a7b484de25fb4f0dc4e8c863107bf525ff26e92.jpeg', 449.99),
+('Villa Esmeralda Retreat', 'Escape tranquilo en una villa privada con jardines encantadores y piscina privada', 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/475438288.jpg?k=2a20ee8e033b9c405e27aa94af4512ba69227eaada8114dc2d1ec4bd4953314c&o=&hp=1', 699.99),
+('Grand Cascada Palace', 'Elegante palacio con cascadas, habitaciones lujosas y exclusivo servicio de mayordomo', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/hotelier-images/c9/9b/a9081bffbe2fbc0daac7d68b73d8715e0a3c4c189322ea7cb81037564c41.jpeg', 799.99),
+('Selva Encantada Lodge', 'Inmersión en la selva con bungalows elevados, puentes colgantes y observación de vida silvestre', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partner-images/c3/8f/4fa0652c2d188ec387b5cdb9a0b90a984e8bdc5d56ddb979a32a616ec983.jpeg', 459.99),
+('Playa Dorada Resort', 'Refugio en la playa con arenas doradas, deportes acuáticos y entretenimiento nocturno', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/hotelier-images/28/4d/004706361c19284124bc26d9042bcaf81d0c77b72accac0482b5d0a7bac8.jpeg', 529.99),
+('Majestic Heights Hotel', 'Hotel en la cima de una montaña con vistas panorámicas, spa de altitud y cocina de autor', 'https://hotelvillasoro.com/backoffice/images/834-184vs1300x870.jpg?_gl=1*npc7bh*_ga*MTYzMDMzMTI3NC4xNzA1NTgzNzkw*_ga_XY2W2CCFN7*MTcwNTU4Mzc5MC4xLjEuMTcwNTU4MzgwNi40NC4wLjA', 679.99),
+('Río Serpenteante Retreat', 'Retiro junto al río con cabañas rústicas, fogatas nocturnas y actividades de pesca', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/b6/5b/80/hotel-chiqui.jpg?w=1200&h=-1&s=1', 389.99),
+('Azure Bay Resort', 'Resort junto a la bahía con aguas cristalinas, villas sobre el agua y gastronomía internacional', 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/474542950.jpg?k=288335421cf307a6d028d9b924522e4c551b540e8d5adfed9199ad14f901851a&o=&hp=1', 749.99),
+('Alpes Blancos Lodge', 'Refugio alpino con acceso a pistas de esquí, spa alpino y vistas panorámicas de montañas nevadas', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/99/1e/19/vista-dal-solarium-piscina.jpg?w=1200&h=-1&s=1', 599.99),
+('Tranquil Pines Inn', 'Inn pintoresco en el bosque con cabañas de madera, senderos naturales y fogatas al aire libre', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/03/43/73/mirador-champagne-bar.jpg?w=1200&h=-1&s=1', 329.99),
+('Palacio del Desierto Oasis', 'Experiencia de lujo en el desierto con palacios privados, paseos en camello y cenas bajo las estrellas', 'https://viajerosocultos.com/wp-content/uploads/2023/02/hermosa-vista-isla-fatnas-siwa-egipto.jpg', 899.99),
+('Estrella Fugaz Lodge', 'Retiro nocturno con telescopios astronómicos, cúpulas para observar las estrellas y actividades astrales', 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/226982045.jpg?k=dd67aac45b75a048b109083640d842ee0017e301cc8cd335fe46570d7ee7d124&o=&hp=1', 469.99),
+('Granja Serena Bed & Breakfast', 'Encanto rural en una granja con desayunos caseros, huertos y granja interactiva', 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/185716955.jpg?k=574fe448f37d2f738b22de9be4b319ceb85bd7809fc3d95ce83c1d11e8f89015&o=&hp=1', 249.99),
+('Riviera Bliss Hotel', 'Hotel elegante en la Riviera con acceso privado a la playa, spa y gastronomía mediterránea', 'https://www.kayak.es/rimg/himg/c1/66/dc/expediav2-2501445-3682287276-253776.jpg?width=968&height=607&crop=true', 579.99),
+('Trópico Encantado Resort', 'Resort tropical con palapas privadas, actividades acuáticas y exuberantes jardines tropicales', 'https://www.kayak.es/c/wp-content/uploads/sites/200/2023/08/squircle-1.png', 509.99),
+('Cielo Estrellado Inn', 'Encanto bajo las estrellas en un acogedor bed & breakfast con vistas nocturnas impresionantes','https://www.kayak.es/rimg/himg/b5/ab/78/expediav2-7523182-4237258353-805670.jpg?width=968&height=607&crop=true' , 319.99),
+('Marino Azul Retreat', 'Retiro costero con cabañas sobre pilotes, pesca en alta mar y cocina marinera de primera clase', 'https://xx.bstatic.com/xdata/images/xphoto/1182x887/281113497.jpg?k=e8e1e30d2507a756280e84bceb323ca1b775dbc47f6fc9b692bc7fe045ee8ca0&o=?size=M', 689.99),
+
+
+
+
+
+
