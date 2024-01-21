041b79916d889272f7b0c27928cdeb4a5517b690
evavelasco <evapibiyal@gmail.com>
2024-01-21 18:09:53 +0100

    snackbar

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,15 +4,9 @@ VALUES
 ('Hotel Campo Verde', 'Hotel tranquilo en el campo con vistas a la montaña, ideal para escapadas románticas', 'https://static.leonardo-hotels.com/image/P_LHBLR_05_may16_4000x2600_8254e8943e796f7d44a32ffd8d650a25_1200x780_mobile_3.webp', 99.89),
 ('Urbano Moderno', 'Hotel boutique en el centro de la ciudad con decoración moderna y un bar en la azotea', 'https://static.leonardo-hotels.com/image/p_lrhbf_02_nov21_-4000x2600_0039400c1259e6b7fe1a79ef368294b9_1200x780_mobile_3.webp', 199.20),
 ('Refugio de Montaña', 'Hotel en la montaña con acceso a senderos de trekking y vistas panorámicas', 'https://www.estiber.com/blog/wp-content/uploads/2023/07/refugio-lluis-estasen.jpg', 37.99),
-<<<<<<< HEAD
 ('Almeida 1',  'Encanto y comodidad en Almeida de Sayago, habitación individual.', 'https://imgcy.trivago.com/c_fill,d_dummy.jpeg,f_auto,h_190,q_auto,w_240//partner-images/c5/15/b5a03496961d97fb674c9b673e640b1badae39c36adcbdf4420c770236bf.jpeg', 90),
 
  ('Sunset Paradise Hotel', 'Hotel boutique en la costa con impresionantes vistas al atardecer, piscina infinita y spa de lujo', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/db/f9/eb/limanaki-hotel.jpg?w=1200&h=-1&s=1', 599.99),
-=======
-('Almeida 1', 'Encanto y comodidad en Almeida de Sayago, habitación individual.', 'https://imgcy.trivago.com/c_fill,d_dummy.jpeg,f_auto,h_190,q_auto,w_240//partner-images/c5/15/b5a03496961d97fb674c9b673e640b1badae39c36adcbdf4420c770236bf.jpeg', 90),
-('Almeida 2', 'Encanto y comodidad en Almeida de Sayago, habitación doble.', 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/5011469.jpg?k=89074ae1dae1cc2daa7c3b2c1f7fbad78d0a0a1fb43b30c6363828a2b24f8b68&o=&hp=1', 120),
-('Sunset Paradise Hotel', 'Hotel boutique en la costa con impresionantes vistas al atardecer, piscina infinita y spa de lujo', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/db/f9/eb/limanaki-hotel.jpg?w=1200&h=-1&s=1', 599.99),
->>>>>>> fd3383fb83a73a9b795257a0e3129c1a48e0bb22
 ('Montaña Mágica Lodge', 'Retiro en la montaña rodeado de bosques, cabañas acogedoras y senderos naturales', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/d3/03/43/la-montana-magica.jpg?w=1200&h=-1&s=1', 349.99),
 ('Oasis Sereno Resort', 'Resort de lujo con jardines exuberantes, spa rejuvenecedor y cocina gourmet', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partner-images/96/e2/4529aecaa4a6578881485c3078756ee3a31d7b6a7259962bf8887a523e19.jpeg', 499.99),
 ('Aventura Marina Hotel', 'Experimenta la vida marina en este hotel frente al océano con actividades acuáticas y restaurantes temáticos', 'https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/partner-images/7d/d4/827377afcea0686d6a599a7b484de25fb4f0dc4e8c863107bf525ff26e92.jpeg', 449.99),
@@ -32,13 +26,10 @@ VALUES
 ('Trópico Encantado Resort', 'Resort tropical con palapas privadas, actividades acuáticas y exuberantes jardines tropicales', 'https://www.kayak.es/c/wp-content/uploads/sites/200/2023/08/squircle-1.png', 509.99),
 ('Cielo Estrellado Inn', 'Encanto bajo las estrellas en un acogedor bed & breakfast con vistas nocturnas impresionantes','https://www.kayak.es/rimg/himg/b5/ab/78/expediav2-7523182-4237258353-805670.jpg?width=968&height=607&crop=true' , 319.99),
 ('Marino Azul Retreat', 'Retiro costero con cabañas sobre pilotes, pesca en alta mar y cocina marinera de primera clase', 'https://xx.bstatic.com/xdata/images/xphoto/1182x887/281113497.jpg?k=e8e1e30d2507a756280e84bceb323ca1b775dbc47f6fc9b692bc7fe045ee8ca0&o=?size=M', 689.99);
-<<<<<<< HEAD
 
 
 
 
-=======
->>>>>>> fd3383fb83a73a9b795257a0e3129c1a48e0bb22
 
 
 
--- frontend/src/components/Modal/ModalReservationDates.jsx
+++ frontend/src/components/Modal/ModalReservationDates.jsx
@@ -24,7 +24,7 @@ export const ModalReservationDates = ({
 ModalReservationDates.propTypes = {
   closeModal: PropTypes.func.isRequired,
   hotelId: PropTypes.number.isRequired,
-  setSnackbarOpen: PropTypes.func.isRequired,
+
   name: PropTypes.string.isRequired,
   photoUrl: PropTypes.string.isRequired,
 };
